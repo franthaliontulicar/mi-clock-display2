5866450320c375ae51cd7da27dda52e8ab1af93d
fran <franmenendezpouso@gmail.com>
2015-11-20 11:06:25 +0100

    metodo doceHoras

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -11,68 +11,75 @@ public class ClockDisplay
     private NumberDisplay hora;
     private NumberDisplay minuto;
     private String time;
-   
-    
-    public ClockDisplay(){
-    
-        hora = new NumberDisplay(24);
-        minuto = new NumberDisplay(60);
+    private boolean franja; 
 
-           
-        time = hora.getDisplayValue() + ":" + minuto.getDisplayValue();
-    
-    }
-    
-    public ClockDisplay(int horaDada, int minutoDado){
-     
+    public ClockDisplay(boolean doceHoras){
         hora = new NumberDisplay(24);
         minuto = new NumberDisplay(60);
+        franja = doceHoras;
+
+        time = hora.getDisplayValue() + ":" + minuto.getDisplayValue();
+    }
+    public ClockDisplay(int horaDada, int minutoDado, boolean doceHoras){
+        hora = new NumberDisplay(24);
+        minuto = new NumberDisplay(60);
+        franja = doceHoras;
         hora.setValue(horaDada);
         minuto.setValue(minutoDado);
         time = hora.getDisplayValue() + ":" + minuto.getDisplayValue();
-    
+
     }
-    
-    
+
     
     public void setTime(int valorHora, int valorMinuto){
-       
+
         hora.setValue(valorHora);
         minuto.setValue(valorMinuto);
-        
+
         time = hora.getDisplayValue() + ":" + minuto.getDisplayValue();  
     }
-    
-    
+
     public String getTime(){
-        String tiempo = hora.getDisplayValue();
-        String mañana = " am";
-        
-        if(hora.getValue() > 12 && hora.getValue() <= 22){
-            tiempo = "0" + hora; 
-        
-        } 
-       
-        if(hora.getValue() > 12) {
-            tiempo = (hora.getValue() - 12) + ""; 
-        }
-         mañana = "pm";
-         if( hora.getValue() == 0){
-            tiempo = "12";
-            }
-        
-         if(hora.getValue() == 12 && minuto.getValue() ==0 ){
-            mañana = "m";
-            }
-        time = tiempo + ":" + minuto.getDisplayValue() + mañana;  
         return time;
     }
-    
+
     public void timeTick(){
         minuto.increment();          
         if(minuto.getValue() == 0){
-             hora.increment();
-            }
-        time = hora.getDisplayValue() + ":" + minuto.getDisplayValue();
+            hora.increment();
         }
+        time = hora.getDisplayValue() + ":" + minuto.getDisplayValue();
+    }
+
+    /**
+     * actualiza el reloj a modo de 12 horas
+     * /
+     */
+    public void doceHoras(){
+        String tiempo = hora.getDisplayValue();
+        String mañana = " am";
+
+        if((hora.getValue() > 12) && (hora.getValue() <= 22)){
+            tiempo = "0" + hora; 
+
+        } 
+
+        if(hora.getValue() >= 12) {
+            if(hora.getValue() > 12) {
+                tiempo = (hora.getValue() - 12) + ""; 
+            }
+            mañana = "pm";
+        }
+
+        if( hora.getValue() == 0){
+            tiempo = "12";
+        }
+
+        if((hora.getValue() == 12) && (minuto.getValue() == 0) ){
+            mañana = "m";
+        }
+        time = tiempo + ":" + minuto.getDisplayValue() + mañana;  
+
+    } 
+
 }
